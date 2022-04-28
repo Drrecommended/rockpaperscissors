93c0a0928ba5996016ce6e314ac16ec444d340b2
Tyler French <tylerfrench89@gmail.com>
2022-04-28 11:11:46 -0700

    Add files via upload

--- rockpaperscissors/index.js
+++ rockpaperscissors/index.js
@@ -1,7 +1,46 @@
+//Play game
+const game = () => {
+    //Initialize variables
+    let round = 0
+    let roundResult
+    let playerScore = 0
+    let computerScore = 0
+    
+    //Loop for 5-rounds of play
+    for(let i = 0; i < 5; i++) {
+        //Gather new selections for each loop
+        const computerSelection = computerPlay()
+        const playerSelection = getPlayerinput()
+        //Pass the selections into play round function and assign the result to roundResult
+        roundResult = playRound(computerSelection, playerSelection)
+        //If the string returns by roundResult includes win, lose or tie handle accordingly
+        if(roundResult.includes('win')) {
+            playerScore++
+        } else if(roundResult.includes('lose')) {
+            computerScore++
+        } else {
+            console.log('tie!')
+        }
+        //Increment round
+        round++
+    }
+
+    console.log(playerScore, computerScore)
+    //Check who won the round
+    if(playerScore > computerScore ) {
+        console.log('YOU WIN!')
+    } else {
+        console.log('YOU LOSE!')
+    }
+
+}
 
 
+//Gather comp choice
 const computerPlay = () => {
+    //Random choice between 1 - 3
     let choice = Math.floor(Math.random() * 3) + 1
+    //Scenarios
     if(choice === 1) {
         choice = 'rock'
     }
@@ -11,50 +50,46 @@ const computerPlay = () => {
     if(choice === 3)  {
         choice = 'paper'
     }
+    //Return the choice
     return choice
 }
 
-const getPlayerinput = () => {
 
-    choice = parseInt(prompt())
-    if(choice !== "rock" || choice !== "paper" || choice !== "scissors") return
+//Gather computer choice
+const getPlayerinput = () => {
+    //Collect input and trim any extra space
+    let choice = prompt().trim()
+    //If not any of the scenarios return 
+    if(choice !== 'rock' & choice !== 'scissors' & choice !== 'paper') {
+        
+        return
+    }
+    //return "Rock, paper or scissors"
+    return choice
 }
 
-const playRound = (cSelection, pSelection) => {
+
+//Play a round
+const playRound = (cSelection, pSelection) => {   
+    //Compare different case scenarios where the player wins 
     if(pSelection === 'rock' && cSelection === 'scissors' 
         || pSelection === 'paper' && cSelection === 'rock'
         || pSelection === 'scissors' && cSelection === 'paper'
         ) {
+        //Return winning scenario as a string using template literals 
         return `You win!, ${pSelection} beats ${cSelection}`
     }
+    //Tie scenario
     if(pSelection === cSelection) return 'TIE!'
+    //Lose scenario
     return `You lose! ${cSelection} beats ${pSelection}`
 }
 
-
-const game = () => {
-
-
-    let winner = playRound(computerSelection, playerSelection)
-
-    if(winner.includes('win')) {
-        console.log('you win')
-        winner = 'player'
-    } else if(winner.includes('lose')) {
-        console.log('you lose')
-        winner = 'computer'
-    } else {
-        console.log('tie!')
-        winner = 'tie'
-    }
-
-}
-
-
-
-const computerSelection = computerPlay()
-const playerSelection = getPlayerinput()
-
+//Call the function allowing game to start
 game()
+
+
+
+
 
 
